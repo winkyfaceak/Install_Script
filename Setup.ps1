28146d24ed33c035f7dae32ee82f8f8b9246ecc9
winkyface <42068367+winkyfaceak@users.noreply.github.com>
2023-12-12 23:15:22 +0000

    first commit

---
+++ Setup.ps1
@@ -0,0 +1,38 @@
+Write-Host "Welcome James!!!"
+Write-Host "This is a programme that allows you to set up your computer way easier `n"
+Write-Host "Type 1 to continue, or 2 to exit"
+$op1 = Read-Host
+$bool
+if ($op1 -eq 1) {
+    Write-Host "are you sure you want to continue????"
+    Write-Host -NoNewLine "Press any key to continue...";
+    $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown");
+    $bool = $true
+    
+    while ($bool -eq $true) {
+        winget install -e --id TIDALMusicAS.TIDA
+        winget install -e --id Microsoft.PowerToys
+        winget install -e --id Discord.Discord
+        winget install -e --id Logitech.GHUB
+        winget install -e --id Nvidia.GeForceExperience
+        winget install -e --id Valve.Steam
+        winget install -e --id Mozilla.Firefox
+        winget install -e --id Git.Git
+        winget install -e --id subhra74.XtremeDownloadManager
+        winget install -e --id M2Team.NanaZip
+        winget install -e --id Notepad++.Notepad++
+        winget install -e --id Microsoft.VisualStudio.2022.Community
+        winget install -e --id JetBrains.PyCharm.Community
+        winget install -e --id JetBrains.IntelliJIDEA.Community
+        winget install -e --id Microsoft.Sysinternals.ProcessExplorer
+        winget install -e --id Soulseek.SoulseekQt
+        winget install -e --id OliverSchwendener.ueli
+        winget install -e --id REALiX.HWiNFO
+        winget install --id Microsoft.Powershell.Preview --source winget
+        $bool = $false
+    }
+}
+elseif ($op1 -eq 2) {
+    break
+}
+    
